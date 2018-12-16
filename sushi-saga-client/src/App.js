0988e0643d6ff34b0c6d9c14052fad23995b9503
Robin Tram <robin.tram@gmail.com>
2018-12-16 17:10:00 -0500

    Done.

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -6,12 +6,54 @@ import Table from './containers/Table';
 const API = "http://localhost:3000/sushis"
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      sushis: [],
+      currentSushis: [],
+      counter: 4,
+      money: 100
+    }
+  }
+
+  componentDidMount() {
+    fetch(API)
+      .then(res => res.json())
+      .then(data => {
+        let currentFour = data.slice(0, 4)
+        this.setState({
+          sushis: data,
+          currentSushis: currentFour
+        })
+      })
+  }
+
+  onEat = (sushi) => {
+    const copySushis = [...this.state.currentSushis]
+    const currentMoney = this.state.money - sushi.price
+    if (currentMoney > 0) {
+      copySushis[copySushis.indexOf(sushi)] = ""
+      this.setState({
+        currentSushis: [...copySushis],
+        money: currentMoney
+      })
+    }
+  }
+
+  updateCurrentSushis = () => {
+    let counter = this.state.counter + 4
+    let currentFour = this.state.sushis.slice(this.state.counter, counter)
+    this.setState({
+      currentSushis: currentFour,
+      counter: counter
+    })
+  }
 
   render() {
     return (
       <div className="app">
-        <SushiContainer  />
-        <Table />
+        <SushiContainer currentSushis={this.state.currentSushis} onEat={this.onEat} updateCurrentSushis={this.updateCurrentSushis}/>
+        <Table money={this.state.money}/>
       </div>
     );
   }
